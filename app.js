8d9080cb23da6f9050534bc169b92919ebad2c0b
Taha Yıldırım <thyldrm23@gmail.com>
2021-05-16 13:29:07 +0300

    template engine created

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const ejs = require("ejs");
+const path = require("path");
 
 const app = express();
+app.set("view engine","ejs")
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
 
-  const blog = { id: 1, title: "Blog title", description: "Blog description" }
-  res.send(blog)
+  res.render("index");
+})
+app.get('/about', (req, res) => {
+
+  res.render("about");
+})
+app.get('/add_post', (req, res) => {
+
+  res.render("add_post");
+
+})
+app.get('/post', (req, res) => {
+
+  res.render("post");
+
 })
 
 const port = 3000;
