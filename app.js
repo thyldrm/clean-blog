8043af6875dc2251a91c5d17e5d3f2011a974181
Taha Yıldırım <thyldrm23@gmail.com>
2021-05-18 00:08:42 +0300

    task 9

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const mongoose = require("mongoose")
+
 const ejs = require("ejs");
 const path = require("path");
+const Blog = require("./models/Blog")
 
 const app = express();
+
+//Middlewares
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+//DB Connect
+mongoose.connect("mongodb://localhost/cleanblog-test-db",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+//Template Engine
 app.set("view engine","ejs")
 
-app.use(express.static('public'))
 
-app.get('/', (req, res) => {
 
-  res.render("index");
+app.get('/', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.render("index", {
+    blogs
+  });
 })
 app.get('/about', (req, res) => {
 
@@ -18,12 +36,15 @@ app.get('/about', (req, res) => {
 app.get('/add_post', (req, res) => {
 
   res.render("add_post");
-
 })
 app.get('/post', (req, res) => {
 
   res.render("post");
+})
 
+app.post("/blogs", async(req,res) => {
+    await Blog.create(req.body)
+    res.redirect("/")
 })
 
 const port = 3000;
