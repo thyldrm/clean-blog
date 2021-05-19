a2142c66a92180eeeb080d8de5b0f9408cb32b85
Taha Yıldırım <thyldrm23@gmail.com>
2021-05-19 15:00:22 +0300

    task 10

--- app.js
+++ app.js
@@ -29,6 +29,14 @@ app.get('/', async (req, res) => {
     blogs
   });
 })
+
+app.get('/blogs/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id)
+  res.render('post', {
+    blog
+  })
+});
+
 app.get('/about', (req, res) => {
 
   res.render("about");
